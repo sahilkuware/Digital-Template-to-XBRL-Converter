b2ba0b93a0365ae545dcb75ca33a3000035ab6f2
Stuart Rowan <stuart.rowan@strr.org>
2025-06-04 18:11:40 +0200

    ruff format

--- src/mireport/excelprocessor.py
+++ src/mireport/excelprocessor.py
@@ -822,9 +822,21 @@ class ExcelProcessor:
                         self._definedNameToXBRLMap.pop(holder.definedName)
 
     def _createNamedPeriods(self) -> None:
-        potentialPeriodHolders = [ holder for holder in self._definedNameToXBRLMap.values() if holder.concept.isAbstract ]
-        membersWithPotentialPeriods = {dimValue for dimPair in self._presetDimensions.values() for dimValue in dimPair.values()}
-        periodHolders = [ p for p in potentialPeriodHolders if p.concept in membersWithPotentialPeriods ]
+        potentialPeriodHolders = [
+            holder
+            for holder in self._definedNameToXBRLMap.values()
+            if holder.concept.isAbstract
+        ]
+        membersWithPotentialPeriods = {
+            dimValue
+            for dimPair in self._presetDimensions.values()
+            for dimValue in dimPair.values()
+        }
+        periodHolders = [
+            p
+            for p in potentialPeriodHolders
+            if p.concept in membersWithPotentialPeriods
+        ]
         for periodHolder in periodHolders:
             dimValueDN = periodHolder.definedName
             namedPeriod = dimValueDN.name
@@ -833,9 +845,7 @@ class ExcelProcessor:
                 self._definedNameToXBRLMap.pop(dimValueDN)
                 continue
 
-            if (isinstance(year, bool)
-                or not isinstance(year, float | int | str)
-            ):
+            if isinstance(year, bool) or not isinstance(year, float | int | str):
                 self._results.addMessage(
                     f"Unable to extract year for {dimValueDN.name}. Cell value '{year}'",
                     Severity.ERROR,
